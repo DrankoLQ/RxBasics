1b49b173d2c94b6a3b50c6109b5fc75d4dd1026f
Daniel Luque Quintana <dranko91@gmail.com>
2017-09-13 16:44:37 +0200

    Update activity layout

--- app/src/main/java/com/daniellq/rxbasics/MainActivity.java
+++ app/src/main/java/com/daniellq/rxbasics/MainActivity.java
@@ -1,13 +1,25 @@
 package com.daniellq.rxbasics;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnGetData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnGetData = (Button) findViewById(R.id.btnGetData);
+
+        btnGetData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
