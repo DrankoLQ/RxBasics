b3e2f39adafb0c4e16ececb492c7948fd53b4256
Daniel Luque Quintana <dranko91@gmail.com>
2017-09-13 17:01:19 +0200

    Apply butterknife in MainActivity

--- app/src/main/java/com/daniellq/rxbasics/MainActivity.kt
+++ app/src/main/java/com/daniellq/rxbasics/MainActivity.kt
@@ -3,16 +3,17 @@ package com.daniellq.rxbasics
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import butterknife.BindView
+import butterknife.ButterKnife
 
 class MainActivity : AppCompatActivity() {
-    var btnGetData: Button? = null
+    @BindView(R.id.btnGetData)
+    lateinit var btnGetData: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        btnGetData = findViewById(R.id.btnGetData) as Button
-
-        btnGetData?.setOnClickListener { }
+        ButterKnife.bind(this)
+        btnGetData.setOnClickListener { }
     }
 }
